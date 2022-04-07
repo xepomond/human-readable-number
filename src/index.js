26e1b946e141da42496e3858238089bd49875700
xepomond <anka4717@gmail.com>
2022-04-07 21:38:22 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,38 @@
 module.exports = function toReadable (number) {
-  
+      let singleDig = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+      let tenDig = ['ten','eleven','twelve','thirteen', 'fourteen','fifteen','sixteen', 'seventeen','eighteen','nineteen'];
+      let twenDig = ['', '', 'twenty','thirty','forty','fifty', 'sixty','seventy','eighty','ninety'];
+      if (number < 0) {
+         return ('It is a negative number');
+       }
+        else if (number > 100 && number <= 999 && 10 > number % 100 && number % 100 !== 0) {
+         return singleDig[ Math.floor(number / 100)] + ' hundred ' + singleDig[Math.floor(number % 10)]; 
+      }
+        else if (number > 100 && number <= 999 && 20 > number % 100 && number % 100 !== 0) {
+         return singleDig[ Math.floor(number / 100)] + ' hundred ' + tenDig[Math.floor(number % 10)]; 
+      }
+         else if (number > 100 && number <= 999 && number % 10 === 0 && number % 100 !== 0){
+         return singleDig[ Math.floor(number / 100 )] + ' hundred ' + twenDig[ Math.floor(number / 10 % 10)]
+      }
+         else if (number >= 100 && number <= 999 && number % 100 !== 0){
+         return singleDig[ Math.floor(number / 100 )] + ' hundred ' + twenDig[ Math.floor(number / 10 % 10)] + ' ' + singleDig[number % 10];
+       }
+       else if (number >= 100 && number <= 999 && number % 100 === 0){
+         return singleDig[ Math.floor(number / 100 )] + ' hundred';
+       }
+      else if (number >=20 && number <= 99 && number % 10 !== 0) {
+         return twenDig[Math.floor(number / 10)] + ' ' + singleDig[number % 10];
+       }
+      else if (number >=20 && number <= 99 && number % 10 === 0) {
+         return twenDig[Math.floor(number / 10)];
+       }
+      else if (number >= 10 && number <= 19 ) {
+         return tenDig[number % 10 ];
+      }
+      else if (number <= 9  && number > 0) {
+         return singleDig[number];
+      }
+      else if (number === 0) {
+         return 'zero';
+      }
 }
