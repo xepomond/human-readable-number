d1c6b5d70dd17093c486ee830686d6f8eacfb636
xepomond <anka4717@gmail.com>
2022-04-08 20:49:49 +0300

    refactor code

--- src/index.js
+++ src/index.js
@@ -2,37 +2,37 @@ module.exports = function toReadable (number) {
       let singleDig = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
       let tenDig = ['ten','eleven','twelve','thirteen', 'fourteen','fifteen','sixteen', 'seventeen','eighteen','nineteen'];
       let twenDig = ['', '', 'twenty','thirty','forty','fifty', 'sixty','seventy','eighty','ninety'];
-      if (number < 0) {
-         return ('It is a negative number');
-       }
-        else if (number > 100 && number <= 999 && 10 > number % 100 && number % 100 !== 0) {
+
+      if (number < 0) return ('It is a negative number');
+       
+      if (number > 100 && number <= 999 && 10 > number % 100 && number % 100 !== 0) 
          return singleDig[ Math.floor(number / 100)] + ' hundred ' + singleDig[Math.floor(number % 10)]; 
-      }
-        else if (number > 100 && number <= 999 && 20 > number % 100 && number % 100 !== 0) {
-         return singleDig[ Math.floor(number / 100)] + ' hundred ' + tenDig[Math.floor(number % 10)]; 
-      }
-         else if (number > 100 && number <= 999 && number % 10 === 0 && number % 100 !== 0){
-         return singleDig[ Math.floor(number / 100 )] + ' hundred ' + twenDig[ Math.floor(number / 10 % 10)]
-      }
-         else if (number >= 100 && number <= 999 && number % 100 !== 0){
+   
+      if (number > 100 && number <= 999 && 20 > number % 100 && number % 100 !== 0)
+         return singleDig[ Math.floor(number / 100)] + ' hundred ' + tenDig[Math.floor(number % 10)];
+         
+      if (number > 100 && number <= 999 && number % 10 === 0 && number % 100 !== 0) 
+         return singleDig[ Math.floor(number / 100 )] + ' hundred ' + twenDig[ Math.floor(number / 10 % 10)];
+      
+      if (number >= 100 && number <= 999 && number % 100 !== 0) 
          return singleDig[ Math.floor(number / 100 )] + ' hundred ' + twenDig[ Math.floor(number / 10 % 10)] + ' ' + singleDig[number % 10];
-       }
-       else if (number >= 100 && number <= 999 && number % 100 === 0){
+      
+      if (number >= 100 && number <= 999 && number % 100 === 0) 
          return singleDig[ Math.floor(number / 100 )] + ' hundred';
-       }
-      else if (number >=20 && number <= 99 && number % 10 !== 0) {
+
+      if (number >=20 && number <= 99 && number % 10 !== 0) 
          return twenDig[Math.floor(number / 10)] + ' ' + singleDig[number % 10];
-       }
-      else if (number >=20 && number <= 99 && number % 10 === 0) {
+      
+      if (number >=20 && number <= 99 && number % 10 === 0) 
          return twenDig[Math.floor(number / 10)];
-       }
-      else if (number >= 10 && number <= 19 ) {
+       
+      if (number >= 10 && number <= 19 ) 
          return tenDig[number % 10 ];
-      }
-      else if (number <= 9  && number > 0) {
+      
+      if (number <= 9  && number > 0) 
          return singleDig[number];
-      }
-      else if (number === 0) {
+      
+      if (number === 0) 
          return 'zero';
-      }
+      
 }
